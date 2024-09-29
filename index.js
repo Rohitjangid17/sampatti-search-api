efc4bba6876664aacff1a26801b1cecbf7c4b44f
Rohitjangid17 <jangidrohit817@gmail.com>
2024-09-30 00:12:21 +0530

    add swagger ui url

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:5000/"
+                url: "https://sampatti-search-api.vercel.app/api-docs/"
             }
         ]
     },
