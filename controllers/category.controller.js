ae99fe04e694912816f8333ddc3229b48f2d3740
Rohitjangid17 <jangidrohit817@gmail.com>
2024-12-04 14:26:11 +0530

    add new chages

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -17,36 +17,36 @@ export const createCategory = async (req, res) => {
         if (existingCategory) {
             response = false;
             message = "Category is already taken";
-            res.status(400).json({ response: response, message: message });
+            return res.status(400).json({ response: response, message: message });
         }
 
         // check title is required
         if (!title) {
             response = false;
             message = "Category title is required";
-            res.status(400).json({ response: response, message: message });
+            return res.status(400).json({ response: response, message: message });
         }
 
         // check title length grether then 100 character or not
         if (title.length > 100) {
             response = false;
-            message = "Category title should not exceed 100 characters";
+            message = "Title too long! Please keep it under 100 characters.";
             return res.status(400).json({ response: response, message: message });
         }
 
         // check description length grether then 500 character or not
         if (description && description.length > 500) {
             response = false;
-            message = "Category description should not exceed 500 characters";
+            message = "Description too long! Please keep it under 500 characters";
             return res.status(400).json({ response: response, message: message });
         }
 
         await category.save();
         response = true;
         message = "Category created successfully";
-        res.status(201).json({ response: response, message: message });
+        return res.status(201).json({ response: response, message: message });
     } catch (error) {
-        res.status(500).json({ response: response, error: error.message })
+        return res.status(500).json({ response: response, error: error.message })
     }
 }
 
@@ -64,7 +64,7 @@ export const getCategory = async (req, res) => {
             if (!category) {
                 response = false;
                 message = "Category not found";
-                res.status(404).json({ response: response, message: message });
+                return res.status(404).json({ response: response, message: message });
             }
             return res.status(200).json(category);
         }
@@ -76,11 +76,11 @@ export const getCategory = async (req, res) => {
         const totalCategory = await Category.countDocuments();
         response = true;
 
-        res.status(200).json({ response: response, categories, total: totalCategory, page, totalPage: Math.ceil(totalCategory / limit) });
+        return res.status(200).json({ response: response, categories, total: totalCategory, page, totalPage: Math.ceil(totalCategory / limit) });
     } catch (error) {
         let response = false;
         let message = "Something went wrong";
-        res.status(500).json({ response: response, message: message, error: error.message });
+        return res.status(500).json({ response: response, message: message, error: error.message });
     }
 }
 
@@ -98,12 +98,12 @@ export const deleteCategoryById = async (req, res) => {
         if (!category) {
             response = false;
             message = "Category not found";
-            res.status(404).json({ response: response, message: message });
+            return res.status(404).json({ response: response, message: message });
         }
 
         response = true;
         message = "Category delete successfully";
-        res.status(200).json({ response: response, message: message });
+        return res.status(200).json({ response: response, message: message });
     } catch (error) {
         response = false;
         message = "Something went wrong";
@@ -128,14 +128,14 @@ export const updateCategoryById = async (req, res) => {
         if (existingCategory) {
             response = false;
             message = "Category is already taken";
-            res.status(400).json({ response: response, message: message });
+            return res.status(400).json({ response: response, message: message });
         }
 
         // check title is required
         if (!title) {
             response = false;
             message = "Category title is required";
-            res.status(400).json({ response: response, message: message });
+            return res.status(400).json({ response: response, message: message });
         }
 
         // check title length grether then 100 character or not
@@ -154,18 +154,19 @@ export const updateCategoryById = async (req, res) => {
 
         const category = await Category.findByIdAndUpdate(id, req.body, { new: true });
 
+        // check category not found
         if (!category) {
             response = false;
             message = "Category not found";
-            res.status(404).json({ response: response, message: message });
+            return res.status(404).json({ response: response, message: message });
         }
 
         response = true;
         message = "Category updated successfully";
-        res.status(200).json({ response: response, message: message });
+        return res.status(200).json({ response: response, message: message });
     } catch (error) {
         let response = false;
         let message = "Something went wrong";
-        res.status(500).json({ response: response, message: message, error: error.message });
+        return res.status(500).json({ response: response, message: message, error: error.message });
     }
 }
